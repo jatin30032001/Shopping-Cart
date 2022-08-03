336ac46bab1c0e967e770082aeb0880d69a777df
jatin30032001 <jrsaini2090@gmail.com>
2022-08-03 20:15:04 +0530

    Add and Remove

--- myapp/src/pages/Cart.js
+++ myapp/src/pages/Cart.js
@@ -1,16 +1,22 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector , useDispatch } from 'react-redux'
+import { remove } from '../Stores/cartSlice';
 
 const Cart = () => {
 
+  const dispatch = useDispatch();
   const products = useSelector((state) => state.cart)
+
+  const handleRemove =(productId ) =>{
+ dispatch(remove(productId));
+  }
 
   return (
     <div>
       <h3>Cart</h3>
       <div className='cartWrapper'>
         {
-               products.map(product=>(
+               products.map((product)=>(
 
 
                 <div className='cartcard'>
@@ -18,7 +24,7 @@ const Cart = () => {
                   <h5>{product.title}</h5>
                   <h5>{product.price}</h5>
 
-                  <button  className='btn'> Remove</button>
+                  <button onClick={()=> handleRemove(product.id)}  className='btn'> Remove</button>
                 </div>
                )
                )
