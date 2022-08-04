831410d86f042f21fda943d07562ad3d9ac32944
jatin30032001 <jrsaini2090@gmail.com>
2022-08-04 15:58:10 +0530

    Final Commit

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,6 +1,5 @@
 
 import './App.css';
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
@@ -9,8 +8,7 @@ import {Provider} from "react-redux"
 
 import store from './Stores/store'
 
-
-
+//App.js
 
 function App() {
   return (
