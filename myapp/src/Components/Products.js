b49330edef0d493d94f5204bf5550bd8aa47f81d
jatin30032001 <jrsaini2090@gmail.com>
2022-08-04 15:55:38 +0530

    Final Commit

--- myapp/src/Components/Products.js
+++ myapp/src/Components/Products.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { add } from '../Stores/cartSlice';
 import { fetchProducts } from '../Stores/productSlice';
@@ -18,7 +18,7 @@ const Products = () => {
         //     setProducts(data);
         // };
         // fetchProducts();
-    }, []);
+    },[]);
 
     const handleAdd = (product) => {
         dispatch(add(product));
